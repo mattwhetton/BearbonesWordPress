76c4da7dc416c3aa2b8691dab8f93096c22d57d4
Humenko <danielhumenko@gmail.com>
2018-07-26 23:09:01 +0100

    initial

--- single.php
+++ single.php
@@ -1,67 +1,37 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package Bearbones
+ */
 
-	<main role="main">
-	<!-- section -->
-	<section>
+get_header();
+?>
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<?php
+		while ( have_posts() ) :
+			the_post();
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+			get_template_part( 'template-parts/content', get_post_type() );
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
+			the_post_navigation();
 
-			<!-- post details -->
-			<span class="date"><?php the_time('F j, Y'); ?> <?php the_time('g:i a'); ?></span>
-			<span class="author"><?php _e( 'Published by', 'bearbones' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'bearbones' ), __( '1 Comment', 'bearbones' ), __( '% Comments', 'bearbones' )); ?></span>
-			<!-- /post details -->
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
 
-			<?php the_content(); // Dynamic Content ?>
+		endwhile; // End of the loop.
+		?>
 
-			<?php the_tags( __( 'Tags: ', 'bearbones' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
 
-			<p><?php _e( 'Categorised in: ', 'bearbones' ); the_category(', '); // Separated by commas ?></p>
-
-			<p><?php _e( 'This post was written by ', 'bearbones' ); the_author(); ?></p>
-
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
-
-			<?php comments_template(); ?>
-
-		</article>
-		<!-- /article -->
-
-	<?php endwhile; ?>
-
-	<?php else: ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php _e( 'Sorry, nothing to display.', 'bearbones' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
-
-<?php get_footer(); ?>
+<?php
+get_sidebar();
+get_footer();
