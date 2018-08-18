b19e52c6970372b988da04f1aad26291e84c94d4
Matt <matthew.whetton@gmail.com>
2018-08-18 18:36:56 +0100

    Remove overkill content from home page

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -20,27 +20,6 @@
 			the_title( '<h1><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
 		?>
 			</div>
-			<div class="subtitle">
-				<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'bearbones' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
-
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bearbones' ),
-			'after'  => '</div>',
-		) );
-		?>
-			</div>
 		</div>
 		<!-- .content -->
 
